2182b5f70e95b9e2246ae2db794f6f247e6f915b
Jenna Xiao <jenna_xiao@hotmail.com>
2022-11-04 18:19:58 -0400

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'; // to add
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState(null);
 
-  React.useEffect(() => {
+  useEffect(() => {
+
+    // For GET
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
+
+    // For POST
+    let inBody = { // actual data
+      name: 'Donald',
+      like: 'ducks'
+    }
+    const someStuff = { // request information (metadata)
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(inBody)
+    };
+    fetch("/hello", someStuff)
+          .then(res => res.json())
+          .then((data) => setData(data.message));
   }, []);
 
   return (
--- server/index.js
+++ server/index.js
@@ -1,12 +1,47 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+const admin = require('firebase-admin'); // to add
+//const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const serviceAccount = require('./playground-7e100-firebase-adminsdk-7tucq-8c7f70171e.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+// module.exports.db = db;
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.post("/hello", (req, res) => {
+  let myName = req.body.name;
+  let myLiking = req.body.like;
+
+  res.json({ message: "I'm " + myName + " and I like " + myLiking });
+})
+
+//creates a new user with firstName, lastName, and email
+app.post('/setUsers', async function (req, res) {
+  try{
+    console.log(req.body);
+    await db.collection("users").doc(req.body.email).set(req.body);
+    res.send(req.body)
+  } catch (err){
+    res.send(err)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
